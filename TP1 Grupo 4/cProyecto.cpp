d2b9b676595cfe1a5d4628d4c5630493bb7d207a
vickytolosa <73183305+vickytolosa@users.noreply.github.com>
2022-09-16 14:52:29 -0300

    Update cProyecto.cpp

--- TP1 Grupo 4/cProyecto.cpp
+++ TP1 Grupo 4/cProyecto.cpp
@@ -8,11 +8,32 @@ using namespace std;
 
 cProyecto::cProyecto(string Nombre, int ID_Proyecto, eEstado estado, int Etapa)
 {
-	this-> string Nombre = _nombre;
-	this-> int ID_Proyecto = _id;
-	this-> eEstado estado = _estado;
-	this-> int Etapa = _etapa;
+	this-> Nombre = Nombre;
+	this-> ID_Proyecto = ID_Proyecto;
+	this-> estado = estado;
+	this-> Etapa = etapa;
+	//deberiamos agregar un enum para las etapas, o definir que representa cada numero, si es así siempre inicializariamos la etapa como 0
+	//Etapa=0;
+	
+	ListaEntregasRealizadas = new cEntregas * [maxproyectos];
+	for (int i = 0; i < maxproyectos; i++)
+	{
+		ListaProyectos[i] = NULL;
+	}
+
 
 }
 
 cProyecto::~cProyecto() {}
+
+void cProyecto::ReasignarProyecto(cJefes* NuevoLider)
+{
+	this->lider = NuevoLider;
+
+}
+
+void cProyecto::RecibirEntrega()
+{
+
+
+}
