90552cc2f5508afb2c9f402a1efb1b02e86ae157
vickytolosa <73183305+vickytolosa@users.noreply.github.com>
2022-09-16 14:21:56 -0300

    Update cEntregas.cpp

--- TP1 Grupo 4/cEntregas.cpp
+++ TP1 Grupo 4/cEntregas.cpp
@@ -1,1 +1,23 @@
 #include "cEntregas.h"
+
+cEntregas::cEntregas(int Num_etapa, int ID, bool Aceptada)
+{
+	this->Num_Etapa = Num_etapa;
+	this->ID_Proyecto = ID;
+	this->Aceptada = Aceptada;
+}
+
+void cEntregas::Actualizar(cJefes* jefe)
+{
+	CantEntregas = CantEntregas + 1;
+	if(Aceptada==1)
+	{
+		
+	//indicar finalizacíon
+
+	}
+	else
+	{
+	//imprimir slicitud de re entrega¨)
+	}
+}
